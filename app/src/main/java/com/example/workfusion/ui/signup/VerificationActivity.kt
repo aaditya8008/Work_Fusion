fa75e86ae60e7bf07c3ec86648e63ee12958406d
aaditya8008 <114769448+aaditya8008@users.noreply.github.com>
2024-11-16 13:33:32 +0530

    Verification screen added

--- app/src/main/java/com/example/workfusion/ui/signup/VerificationActivity.kt
+++ app/src/main/java/com/example/workfusion/ui/signup/VerificationActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.os.Handler
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.workfusion.LoadingFragement
 import com.example.workfusion.MainActivity
+import com.example.workfusion.R
 import com.example.workfusion.databinding.ActivityVerificationBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -18,6 +20,19 @@ class VerificationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityVerificationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val fragment = LoadingFragement()
+
+// Get the FragmentManager
+        val fragmentManager = supportFragmentManager
+
+// Begin the transaction
+        val transaction = fragmentManager.beginTransaction()
+
+// Replace the container with the fragment
+        transaction.replace(R.id.fragmentView, fragment)
+
+// Commit the transaction
+        transaction.commit()
 
         // Check email verification every 5 seconds
         val handler = Handler()
